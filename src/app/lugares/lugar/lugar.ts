05ae30bc809cbdc38aac5dac67c2d96bd5bd2484
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-04-28 20:10:23 -0300

    services, modules e component

---
+++ src/app/lugares/lugar/lugar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lugar',
+  standalone: false,
+  templateUrl: './lugar.html',
+  styleUrl: './lugar.css',
+})
+export class Lugar {}
