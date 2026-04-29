2741bf2e8f6a85b7ab0d1595a9c0d11e4e7a1f9f
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-04-29 19:56:37 -0300

    parte 1 componentes

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, } from "@angular/forms"
+import { Categoria } from '../../categorias/categoria';
 
 @Component({
   selector: 'app-lugar',
@@ -6,4 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './lugar.html',
   styleUrl: './lugar.css',
 })
-export class LugarComponent {}
+export class LugarComponent {
+  
+  camposForm: FormGroup
+  categorias: Categoria[] = []
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+    })
+  }
+
+  salvar() {
+    console.log("Enviando formulario: ", this.camposForm.value )
+  }
+}
