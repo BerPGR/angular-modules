852ddc129fbeea29b55a4c8d009c98f72d3da66c
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-05-01 17:16:58 -0300

    galeria

--- src/app/galeira/galeria/galeria.component.ts
+++ src/app/galeira/galeria/galeria.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LugarClass } from '../../lugares/lugar.class';
+import { HttpClient } from '@angular/common/http';
+import { Categoria } from '../../categorias/categoria';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria.component',
@@ -6,4 +11,28 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css',
 })
-export class GaleriaComponent {}
+export class GaleriaComponent implements OnInit {
+
+  lugares: LugarClass[] = []
+  categoriasFiltro: Categoria[] = []
+
+  constructor(
+    private http: HttpClient,
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: cats => this.categoriasFiltro = cats
+    })
+
+    this.lugarService.obterTodos().subscribe({
+      next: lugares => this.lugares = lugares
+    })
+  }
+
+  getTotalEstrelas(lugar: LugarClass): string {
+    return '&#9733;'.repeat(parseInt(lugar.avaliacao!) || 0)
+  }
+}
